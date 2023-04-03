9a2c0083cc841300a48421eecd3c531b83e64f83
Javi Perez <javi9876789@gmail.com>
2023-04-03 16:18:31 -0600

    Creacion de rutas de las APIs

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,24 @@
 <?php
 
-use App\Http\Controllers\ComparativoController;
-use App\Http\Controllers\ImpresoraController;
-use App\Http\Controllers\LaptopController;
+// use App\Http\Controllers\ComparativoController;
+// use App\Http\Controllers\ImpresoraController;
+// use App\Http\Controllers\LaptopController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LinkController;
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ComparativoController;
+use App\Http\Controllers\PcController;
+use App\Http\Controllers\ImpresoraController;
 use App\Http\Controllers\MonitorController;
 use App\Http\Controllers\PantallaController;
-use App\Http\Controllers\PcController;
 use App\Http\Controllers\ProyectorController;
 use App\Http\Controllers\ScannerController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaptopController;
 
+
+// api de links
 Route::apiResource('marcas', MarcaController::class);
 Route::apiResource('detalles', LinkController::class);
 Route::apiResource('user',UserController::class);
@@ -20,11 +26,27 @@ Route::put('off/{id}',[LinkController::class,'off']);
 Route::put('on/{id}',[LinkController::class,'on']);
 Route::get('cva',[LinkController::class, 'cva']);
 Route::get('allUser',[UserController::class, 'getUser']);
-Route::apiResource('comparativos',[ComparativoController::class]);
-Route::apiResource('impresora',[ImpresoraController::class]);
-Route::apiResource('laptop',[LaptopController::class]);
-Route::apiResource('monitor',[MonitorController::class]);
-Route::apiResource('pantalla',[PantallaController::class]);
-Route::apiResource('pc',[PcController::class]);
-Route::apiResource('proyector',[ProyectorController::class]);
-Route::apiResource('scanner',[ScannerController::class]);
+
+// api de comparativas
+Route::post('gimpresora',[ComparativoController::class,'guardarImpresora']);
+Route::post('glaptop',[ComparativoController::class,'guardarLaptop']);
+Route::post('gmonitor',[ComparativoController::class,'guardarMonitor']);
+Route::post('gpantalla',[ComparativoController::class,'guardarPantalla']);
+Route::post('gpc',[ComparativoController::class,'guardarPc']);
+Route::post('gproyector',[ComparativoController::class,'guardarProyector']);
+Route::post('gscanner',[ComparativoController::class,'guardarScanner']);
+Route::get('compscanners',[ComparativoController::class,'busqscanners']);
+Route::get('compimpresoras',[ComparativoController::class,'busqimpresoras']);
+Route::get('complaptops',[ComparativoController::class,'busqlaptops']);
+Route::get('compmonitores',[ComparativoController::class,'busqmonitores']);
+Route::get('comppantallas',[ComparativoController::class,'busqpantallas']);
+Route::get('comppcs',[ComparativoController::class,'busqpcs']);
+Route::get('compproyectores',[ComparativoController::class,'busqproyectores']);
+Route::get('limpresora',[ImpresoraController::class,'index']);
+Route::get('llaptop',[LaptopController::class,'index']);
+Route::get('lmonitor',[MonitorController::class,'index']);
+Route::get('lpantalla',[PantallaController::class,'index']);
+Route::get('lpcs',[PcController::class,'index']);
+Route::get('lpryector',[ProyectorController::class,'index']);
+Route::get('lscanner',[ScannerController::class,'index']);
+Route::get('viewcomparativos',[ComparativoController::class,'index']);
